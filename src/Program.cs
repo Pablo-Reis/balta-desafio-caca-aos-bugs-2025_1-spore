8197f75d8004ec4f861ded2d4cf9ea2b82659de8
Sarah Guimaraes <saramg12s@gmail.com>
2025-10-26 18:04:34 -0300

    Feats: - Correção de inclusão de DbContextOptions no ctor do AppDbContext. - Inclusão do AppDbContext no container DI. - Geração de migration e banco.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,10 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
